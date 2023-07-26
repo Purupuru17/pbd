e3a4cf124d607954a5f802eaa2c3cdd883bfe587
Lenovo <galihbayu17@gmail.com>
2023-07-26 16:54:27 +0900

    Final Prak

--- tambah.php
+++ tambah.php
@@ -4,19 +4,19 @@
      
      <div class="form-group">
           NIK : 
-          <input class="form-control" name="nik" type="number" value="" placeholder="Isi NIK" />
+          <input required class="form-control" name="nik" type="number" value="" placeholder="Isi NIK" />
      </div>
      <div class="form-group">
           Nama Lengkap : 
-          <input class="form-control" name="nama" type="text" value="" placeholder="Isi Nama" />
+          <input required class="form-control" name="nama" type="text" value="" placeholder="Isi Nama" />
      </div>
      <div class="form-group">
           Tempat Lahir : 
-          <input class="form-control" name="tempat" type="text" value="" placeholder="Isi Kota" />
+          <input required class="form-control" name="tempat" type="text" value="" placeholder="Isi Kota" />
      </div>
      <div class="form-group">
           Tanggal Lahir : 
-          <input class="form-control" name="tanggal" type="date" value="" placeholder="Isi Tanggal" />
+          <input required class="form-control" name="tanggal" type="date" value="" placeholder="Isi Tanggal" />
      </div>
      <div class="form-group">
           <input class="btn btn-success" type="submit" value="Simpan" />
